de0eea760246a32f36cf7096a0ef8e5a5e1ca181
appsu-mi <natubate870@gmail.com>
2026-06-09 14:11:53 +0900

    default_localeをjaに

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,8 @@ module MyYoutubeFeed
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.i18n.default_locale = :ja
+
     config.generators do |g|
       g.test_framework :rspec
       g.fixture_replacement :factory_bot, dir: 'spec/factories'
